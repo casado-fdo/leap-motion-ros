e6e948512f5bd99381a3f7dd7b6252315ae34f03
casado-fdo <casado.fdo@gmail.com>
2025-02-04 17:32:41 +0000

    Add left and hand pose tracking together with grabbing. Also, first visualisation on Rviz is provided

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 start:
+	@xhost +si:localuser:root >> /dev/null
 	cpk run -L local --net host -- --privileged \
+		-e DISPLAY \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-v /dev:/dev \
 		-v ./packages:/code/src/leapmotion-docker/packages
 rviz:
 	cpk run -f -n dev -c bash -M -X --net host -- --privileged 
--- packages/leap-motion-controller/scripts/lmc.py
+++ packages/leap-motion-controller/scripts/lmc.py
@@ -1,36 +1,48 @@
 #!/usr/bin/env python
 import leap
 import rospy
-from geometry_msgs.msg import Point, Pose, Quaternion, PoseStamped
-from lmc_msgs.msg import Hand, Hands
+import tf2_ros
+from geometry_msgs.msg import Point, Quaternion, PoseStamped, TransformStamped
+from sensor_msgs.msg import Range
 from std_msgs.msg import Header
 
+
+DEFAULT_BASE_LINK = 'leap_base_link'
 
 class LeapMotionController(leap.Listener):
     def __init__(self):
         super().__init__()
         rospy.init_node('lmc')
-        #self.pub = rospy.Publisher('~hands', Hands, queue_size=10)
-        self.pub = rospy.Publisher('/hands', PoseStamped, queue_size=10)
+        self.base_link = rospy.get_param('~base_link', DEFAULT_BASE_LINK)
+        self.left_link = "leap_left_hand"
+        self.right_link = "leap_right_hand"
+        self.pub_left = rospy.Publisher('/hands/left/pose', PoseStamped, queue_size=10)
+        self.pub_right = rospy.Publisher('/hands/right/pose', PoseStamped, queue_size=10)
+        self.pub_left_grab = rospy.Publisher('/hands/left/grab', Range, queue_size=10)
+        self.pub_right_grab = rospy.Publisher('/hands/right/grab', Range, queue_size=10)
+
+        # Define a TF link for each hand with base_link as parent
+        self.tfBuffer = tf2_ros.Buffer()
+        self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
+        self.tfBroadcaster = tf2_ros.TransformBroadcaster()
+
         rospy.loginfo('LeapMotionController Node is Up!')
         connection = leap.Connection()
         connection.add_listener(self)
         with connection.open():
             connection.set_tracking_mode(leap.TrackingMode.Desktop)
-            #connection.enable_gesture(leap.GestureType.TYPE_SWIPE)
             rospy.spin()
+
+    def leap_to_ros_coords(self, pos, orientation):
+        new_pos = Point(pos.x, -pos.z, pos.y)
+        new_orientation = Quaternion(orientation.x, - orientation.z, orientation.y, orientation.w)
+
+        return new_pos, new_orientation
+    
 
     def on_connection_event(self, event):
         rospy.loginfo('Connected to a Leap Motion Controller.')
 
-    def on_device_event(self, event):
-        with event.device.open():
-            rospy.loginfo(f'Serial: {event.device.get_info().serial}')
-
-    #def on_tracking_event(self, event):
-    #    hands_lst = [Hand(pose=Pose(position=Point(x=hand.palm.position.x, y=hand.palm.position.y, z=hand.palm.position.z), orientation=Quaternion(x=hand.palm.orientation.x, y=hand.palm.orientation.y, z=hand.palm.orientation.z, w=hand.palm.orientation.w)), pinch_distance=hand.pinch_distance, confidence=hand.confidence, visible_time=hand.visible_time) for hand in event.hands]
-    #    hands_msg = Hands(header=Header(seq=event.tracking_frame_id, stamp=rospy.Time.now(), frame_id='lmc'), hands=hands_lst)
-    #    self.pub.publish(hands_msg)
 
     def on_tracking_event(self, frame):
         # Get the most recent frame and report some basic information
@@ -40,60 +52,66 @@ class LeapMotionController(leap.Listener):
               frame.tracking_frame_id, len(frame.hands)))
 
         if len(frame.hands) > 0: #recently changed in API
-            # Get the first hand
-            #we are seeking one left and one right hands
-            there_is_right_hand=False
-            there_is_left_hand=False
-
             for hand in frame.hands:
+                # Get the hand's normal vector and direction
+                normal = hand.palm.normal
+                direction = hand.palm.direction
+                orientation = hand.palm.orientation
+                pos = hand.palm.position
+
+                # Convert from Leap coordinates (right-handed Cartesian coordinate system, Y-axis up) to ROS coordinates (left-handed Cartesian coordinate system, Z-axis up
+                pos, orientation = self.leap_to_ros_coords(pos, orientation)
+
+                time = rospy.Time.now()
+
+                # Broadcast the hand's pose as a TF frame
+                t = TransformStamped()
+                t.header.stamp = time
+                t.header.frame_id = self.base_link
+                t.child_frame_id = self.left_link if str(hand.type) == "HandType.Left" else self.right_link
+                t.transform.translation.x = pos.x / 100
+                t.transform.translation.y = pos.y / 100
+                t.transform.translation.z = pos.z / 100
+                t.transform.rotation.x = orientation.x
+                t.transform.rotation.y = orientation.y
+                t.transform.rotation.z = orientation.z
+                t.transform.rotation.w = orientation.w
+                self.tfBroadcaster.sendTransform(t)
+
+                # Create a PoseStamped message for the hand's pose
+                ros_pose = PoseStamped()
+                ros_pose.header = Header()
+                ros_pose.header.frame_id = self.base_link
+                ros_pose.header.stamp = time
+                ros_pose.pose.position.x = pos.x / 100
+                ros_pose.pose.position.y = pos.y / 100
+                ros_pose.pose.position.z = pos.z / 100
+                ros_pose.pose.orientation.x = orientation.x
+                ros_pose.pose.orientation.y = orientation.y
+                ros_pose.pose.orientation.z = orientation.z
+                ros_pose.pose.orientation.w = orientation.w
+
+                # Get the hand's grab strength
+                grab = Range()
+                grab.header = Header()
+                grab.header.frame_id = self.left_link if str(hand.type) == "HandType.Left" else self.right_link
+                grab.header.stamp = time
+                #grab.radiation_type = Range.ULTRASOUND
+                grab.field_of_view = 1
+                grab.min_range = 0
+                grab.max_range = 1
+                grab.range = hand.grab_strength
+
+                # Publish hand pose and grab strength
                 if str(hand.type) == "HandType.Left":
-                    hand_type = "Left" 
-                    there_is_left_hand=True
-                    self.right_hand=hand
+                    self.pub_left.publish(ros_pose)
+                    self.pub_left_grab.publish(grab)
                 else:
-                    hand_type = "Right"
-                    there_is_right_hand=True
-                    self.left_hand=hand
+                    self.pub_right.publish(ros_pose)
+                    self.pub_right_grab.publish(grab)
 
-            if not there_is_right_hand:
-                self.right_hand=False
 
-            if not there_is_left_hand:
-                self.left_hand=False
 
-            self.hand = frame.hands[0] #old way
-
-            # Check if the hand has any fingers
-            digits = self.hand.digits
-            #if len(digits) > 0:
-            #    for fingerName in self.fingerNames:
-            #        #finger = fingers.finger_type(Leap.Finger.TYPE_THUMB)[0]
-            #        #self.thumb.importFinger(finger)
-            #        finger = fingers.finger_type(getattr(leap.Finger, 'TYPE_%s' % fingerName.upper()))[0]
-            #        getattr(self, fingerName).importFinger(finger)
-
-            # Get the hand's sphere radius and palm position
-            # print "Hand sphere radius: %f mm, palm position: %s" % (self.hand.sphere_radius, hand.palm_position)
-
-            # Get the hand's normal vector and direction
-            normal = self.hand.palm.normal
-            direction = self.hand.palm.direction
-            orientation = self.hand.palm.orientation
-            pos = self.hand.palm.position
-
-            ros_pose = PoseStamped()
-            ros_pose.header = Header()
-            ros_pose.header.frame_id = 'map'
-            ros_pose.header.stamp = rospy.Time.now()
-            ros_pose.pose.position.x = pos.x
-            ros_pose.pose.position.y = pos.y
-            ros_pose.pose.position.z = pos.z
-            ros_pose.pose.orientation.x = orientation.x
-            ros_pose.pose.orientation.y = orientation.y
-            ros_pose.pose.orientation.z = orientation.z
-            ros_pose.pose.orientation.w = orientation.w
-
-            self.pub.publish(ros_pose)
 
 
 if __name__ == '__main__':
